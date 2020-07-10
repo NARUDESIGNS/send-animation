6286dfb080b35bc5e3ad222debe0d4aab1cbe307
Naru Designs <ibeabuchipaul@gmail.com>
2020-07-10 18:38:40 +0100

    added SEND animation

---
+++ send/app.js
@@ -0,0 +1,53 @@
+let sendBtn = document.getElementById('send');
+let message = document.getElementById('message');
+let character1 = document.getElementById('character1');
+let character2 = document.getElementById('character2');
+let package1 = document.getElementById('package1');
+let package2 = document.getElementById('package2');
+let hand1 = document.getElementById('hand1');
+let hand2 = document.getElementById('hand2');
+
+let state = false;
+
+const animate = () => {
+    if(!state){
+        state = true;
+        message.classList.add("slide-down"); 
+        character1.classList.add("slide-right1");
+        character2.classList.add("slide-left");    
+        //deliver package        
+        setTimeout(() => {
+                package1.style.visibility = "hidden"
+                package2.style.visibility = "visible"
+        }, 2550);            
+
+        //after package is been delivered
+        setTimeout(() => {
+            character1.style.transform = "scaleX(-1)";
+            character1.classList.add("slide-left1");
+            //character1.classList.remove("slide-right1");
+        }, 2700);
+
+        setTimeout(() => {
+            character2.style.transform = "scaleX(1)";
+            character2.classList.add("slide-right");
+        }, 3000);
+        
+        setTimeout(() => {
+            package2.style.visibility = "hidden";
+        },4000);
+
+
+        //show "sent" message
+        setTimeout(() => {
+            message.innerText = "SENT";
+            message.classList.add("slide-up");
+        },4200);
+
+        
+
+    }
+}
+
+//send button is clicked
+sendBtn.addEventListener('click', animate);
